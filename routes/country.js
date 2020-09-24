0e2cbb8d4ba5868528a655cacc6894e66d5d7efd
Faeshal <faeshal111@gmail.com>
2020-09-24 13:07:34 +0700

    add multer

--- routes/country.js
+++ routes/country.js
@@ -2,10 +2,15 @@ require("pretty-error").start();
 const express = require("express");
 const router = express.Router();
 const countryController = require("../controllers/country");
+const Multer = require("multer");
 
 router.get("/api/countries", countryController.getContries);
 
-router.post("/api/countries", countryController.createCountry);
+router.post(
+  "/api/countries",
+  Multer({ storage: Multer.memoryStorage() }).single("image"),
+  countryController.createCountry
+);
 
 router.patch("/api/countries/:id", countryController.updateCountry);
 
